c46d166aa3581c6f2e5a3f0ae23e8ee4b0b1d5fe
James Jarvis <jj@aprsworld.com>
2018-01-04 19:39:22 -0600

    Basic structures in place

---
+++ dynoHR3.c
@@ -0,0 +1,215 @@
+#define _GNU_SOURCE
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+
+#include <mosquitto.h>
+#include <modbus.h>
+
+#define mqtt_host "localhost"
+#define mqtt_port 1883
+
+#define modbus_host "localhost"
+#define modbus_port 502
+#define modbus_slave 40
+
+#define ALARM_SECONDS 10
+
+static int run = 1;
+modbus_t *mb;
+
+void modbus_periodic_query(void) {
+	uint16_t registers[2];
+	int n;
+
+	fprintf(stderr,"# modbus_periodic_query()\n");
+
+	/* cancel pending alarm */
+	alarm(0);
+
+	if ( -1 != modbus_get_socket(mb) ) {
+		n = modbus_read_registers(mb,0,2,registers);
+
+		fprintf(stderr,"# modbus_read_registers() returned %d\n",n);
+
+		for ( int i=0 ; i<n ; i++ ) {
+			fprintf(stderr,"#\t[%d] 0x%02x\n",i,registers[i]);
+		}
+		
+	}
+
+
+	/* set an alarm to send a SIGALARM if data not received within alarmSeconds */
+ 	alarm(ALARM_SECONDS);
+}
+
+void handle_signal(int signum) {
+	
+	if ( SIGALRM == signum ) {
+		/* keep alive modbus connection */
+		modbus_periodic_query();
+
+	} else {
+		fprintf(stderr,"# %s signal received. Terminating.\n",strsignal(signum));
+		run = 0;
+		
+
+	}
+
+}
+
+void connect_callback(struct mosquitto *mosq, void *obj, int result) {
+	printf("# connect_callback, rc=%d\n", result);
+}
+
+void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message) {
+	char data[4];
+	bool match = 0;
+	uint16_t value = 0;
+
+	/* cancel pending alarm */
+	alarm(0);
+	/* set an alarm to send a SIGALARM if data not received within alarmSeconds */
+ 	alarm(ALARM_SECONDS);
+
+
+	printf("got message '%.*s' for topic '%s'\n", message->payloadlen, (char*) message->payload, message->topic);
+
+	/* local copy of our message */
+	strncpy(data, (char *) message->payload,3);
+	data[3]='\0';
+
+	
+
+	/* light above big door */
+	mosquitto_topic_matches_sub("refarm/shop/exteriorLightSouth", message->topic, &match);
+	if (match) {
+		printf("got message for exterior light south\n");
+
+		/* connect to modbus gateway */
+		mb = modbus_new_tcp(modbus_host, modbus_port);
+
+//		modbus_set_debug(mb,TRUE);
+		modbus_set_error_recovery(mb,MODBUS_ERROR_RECOVERY_LINK);
+
+		if ( NULL == mb ) {
+			fprintf(stderr,"# error creating modbus instance: %s\n", modbus_strerror(errno));
+			return;
+
+		}
+
+		if ( -1 == modbus_connect(mb) ) {
+			fprintf(stderr,"# modbus connection failed: %s\n", modbus_strerror(errno));
+			modbus_free(mb);
+			return;
+		}
+
+		/* set slave address of device we want to talk to */
+		if ( 0 != modbus_set_slave(mb,modbus_slave) ) {
+			fprintf(stderr,"# modbus_set_slave() failed: %s\n", modbus_strerror(errno));
+			modbus_free(mb);
+			return;
+
+		}
+
+		/* "ON" (case insensitive) sends a value 1, everything else sends value 0 */
+		if ( NULL != strcasestr(data,"ON") ) {
+			value=1;
+		} 
+
+
+		if ( -1 == modbus_write_register(mb,0,value) ) {
+			fprintf(stderr,"# modbus_write_register() failed: %s\n", modbus_strerror(errno));
+		} else {
+			fprintf(stderr,"# wrote %d to register 0\n",value);
+		}
+
+//		modbus_close(mb);
+//		modbus_free(mb);
+
+	}
+
+	
+
+}
+
+int vfd_gs3_init() {
+	/* Set P3.00 "source of operation" to 0x03 "RS-485 with keypad STOP enabled"*/
+	/* Set P4.00 "source of frequency command" to 0x05 "RS-485" */
+}
+
+int vfd_gs3_set_rpm(int rpm) {
+	/* Set P3.16 "desired frequency" to RPM scaled to Hz */
+
+}
+
+int vfd_gs3_command_run() {
+	/* Set P9
+}
+
+
+/* sweep specifications */
+#define DYNO_RPM_START	50	/* RPM */
+#define DYNO_RPM_END	400	/* RPM to end at */
+#define DYNO_RPM_STEP	10	/* RPM step size */
+#define	DYNO_RPM_DWELL	1	/* seconds to dwell at a given RPM */
+
+/* physical dyno configuration */
+#define DYNO_CONFIG_HZ_PER_RPM	(1.0/8.8)	/* Hz on drive motor per RPM on output shaft */
+
+int main(int argc, char **argv) {
+	uint8_t reconnect = true;
+	char clientid[24];
+	struct mosquitto *mosq;
+	int rc = 0;
+
+	fprintf(stderr,"# dynoHR3 start-up\n");
+
+	fprintf(stderr,"# installing signal handlers\n");
+	signal(SIGINT, handle_signal);
+	signal(SIGTERM, handle_signal);
+	signal(SIGALRM, handle_signal);
+
+	fprintf(stderr,"# initializing mosquitto MQTT library\n");
+	mosquitto_lib_init();
+
+	memset(clientid, 0, 24);
+	snprintf(clientid, 23, "dynoHR3_%d", getpid());
+	mosq = mosquitto_new(clientid, true, 0);
+
+	if ( mosq ) {
+		mosquitto_connect_callback_set(mosq, connect_callback);
+		mosquitto_message_callback_set(mosq, message_callback);
+
+		fprintf(stderr,"# connecting to MQTT server %s:%d\n",mqtt_host,mqtt_port);
+		rc = mosquitto_connect(mosq, mqtt_host, mqtt_port, 60);
+
+		mosquitto_subscribe(mosq, NULL, "refarm/shop/#", 0);
+
+		while (run) {
+			rc = mosquitto_loop(mosq, -1, 1);
+
+			if ( run && rc ) {
+				printf("connection error!\n");
+				sleep(10);
+				mosquitto_reconnect(mosq);
+			}
+		}
+		mosquitto_destroy(mosq);
+	}
+
+	fprintf(stderr,"# mosquitto_lib_cleanup()\n");
+	mosquitto_lib_cleanup();
+
+	fprintf(stderr,"# modbus_close() and modbus_free()\n");
+	modbus_close(mb);
+	modbus_free(mb);
+
+	return rc;
+}
+
--- sample-mcc-usb1608fs.c
+++ sample-mcc-usb1608fs.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "pmd.h"
 #include "usb-1608FS.h"
+libusb_device_handle *udev = NULL;
+Calibration_AIN table_AIN[NGAINS_USB1608FS][NCHAN_USB1608FS];
 
 #define CHANNEL_MODE_ANALOG                0
 #define CHANNEL_MODE_FREQUENCY_FROM_ANALOG 1
@@ -10,11 +13,16 @@
 #define VOLTAGE_LOGIC_HIGH 3.0
 #define VOLTAGE_LOGIC_LOW  1.0
 
+#define DAQ_SAMPLE_FREQ 1000	/* Hz */
+#define DAQ_SAMPLE_COUNT 1000	/* number of samples of each channel */
+
 typedef struct { 
 	int mode;
 	double m;	/* m value to scale voltage to get actual output */
 	double b;	/* b value to add to voltage to get actual output */
 	int gain;	/* USB-1608FS gain setting for this ADC input */
+			/* PGA gain (10V, 5V, 2V, 1V) = { BP_10_00V, BP_5_00V, BP_2_00V, BP_1_00V) */
+
 
 	int nSamples;
 
@@ -34,15 +42,12 @@ typedef struct {
 } struct_channel;
 struct_channel ch[NCHAN_USB1608FS];
 
-void init_channels(void) {
+void init_channel_stats(void) {
+	fprintf(stderr,"# init_channel_stats()\n");
 	for ( int i=0 ; i<NCHAN_USB1608FS ; i++ ) {
-		ch[i].mode=CHANNEL_MODE_ANALOG;
-		ch[i].m=1.0;
-		ch[i].b=0.0;
-		ch[i].gain=BP_10_00V;
-
-		ch[i].nSamples=1.0;
+		ch[i].nSamples=0;
 		ch[i].lastLogicLevel=0;
+		ch[i].nFallingEdges=0;
 		ch[i].vMin=100.0;
 		ch[i].vMax=-100.0;
 		ch[i].vSum=0.0;
@@ -52,112 +57,24 @@ void init_channels(void) {
 	}
 }
 
-int main (int argc, char **argv) {
-	int i,j;
+void init_channels(void) {
+	fprintf(stderr,"# init_channels()\n");
+	for ( int i=0 ; i<NCHAN_USB1608FS ; i++ ) {
+		ch[i].mode=CHANNEL_MODE_ANALOG;
+		ch[i].m=1.0;
+		ch[i].b=0.0;
+		ch[i].gain=BP_10_00V;
+	}
+
+	/* init_channel_stats() needs to be called before each scan */
+}
+
+int daq_acquire(void) {
+	int i;
 	int nSample;
 	uint8_t gainArray[NCHAN_USB1608FS];
 	uint16_t data[1000*NCHAN_USB1608FS*2];
-	int count;
 	int options;
-	float freq;
-
-	libusb_device_handle *udev = NULL;
-	int ret;
-	Calibration_AIN table_AIN[NGAINS_USB1608FS][NCHAN_USB1608FS];
-
-
-	/* configure our channels */
-	init_channels();
-	/* rectifier DC voltage */
-	ch[0].mode=CHANNEL_MODE_ANALOG;
-	ch[0].m=20.0;
-	ch[0].b=0.0;
-	ch[0].gain=BP_5_00V;
-
-	/* rectifier DC current */
-	ch[1].mode=CHANNEL_MODE_ANALOG;
-	ch[1].m=1750.0;
-	ch[1].b=0.0;
-	ch[1].gain=BP_1_00V;
-
-	/* field DC voltage */
-	ch[2].mode=CHANNEL_MODE_ANALOG;
-	ch[2].m=20.0;
-	ch[2].b=0.0;
-	ch[2].gain=BP_5_00V;
-
-	/* field DC current */
-	ch[3].mode=CHANNEL_MODE_ANALOG;
-	ch[3].m=1750.0;
-	ch[3].b=0.0;
-	ch[3].gain=BP_1_00V;
-
-	/* turbine output frequency */
-	ch[4].mode=CHANNEL_MODE_FREQUENCY_FROM_ANALOG;
-	ch[4].m=1.0;
-	ch[4].b=0.0;
-	ch[4].gain=BP_5_00V;
-	ch[4].lastLogicLevel=0;
-	ch[4].nFallingEdges=0;
-
-	/* dyno shaft RPM */
-	ch[5].mode=CHANNEL_MODE_FREQUENCY_FROM_ANALOG;
-	ch[5].m=1.0;
-	ch[5].b=0.0;
-	ch[5].gain=BP_5_00V;
-	ch[4].lastLogicLevel=0;
-	ch[4].nFallingEdges=0;
-
-	/* turbine temperature via IR thermocouple */
-	ch[6].mode=CHANNEL_MODE_ANALOG;
-	ch[6].m=100.0;
-	ch[6].b=0.0;
-	ch[6].gain=BP_2_00V;
-
-	/* strain gauge on dyno output shaft */
-	ch[7].mode=CHANNEL_MODE_ANALOG;
-	ch[7].m=1.0;
-	ch[7].b=0.0;
-	ch[7].gain=BP_5_00V;
-
-
-	udev = 0;
-	ret = libusb_init(NULL);
-	if ( ret < 0 ) {
-		perror("libusb_init: Failed to initialize libusb");
-		exit(1);
-	}
-
-
-	if ( (udev = usb_device_find_USB_MCC(USB1608FS_PID, NULL)) ) {
-		printf("USB-1608FS Device is found!\n");
-	} else {
-		printf("No device found.\n");
-		exit(0);
-	}
- 
-	fprintf(stderr,"# Downloading and building calibration table from device ...\n");
-	usbBuildCalTable_USB1608FS(udev, table_AIN);
-
-	// print the table
-	for (i = 0; i < NGAINS_USB1608FS; i++) {
-		for (j = 0; j < NCHAN_USB1608FS; j++) {
-			fprintf(stderr,"calibration table_AIN[%d][%d].slope = %f    table_AIN[%d][%d].offset = %f\n", i, j, table_AIN[i][j].slope, i, j, table_AIN[i][j].offset);
-		}	
-	}
-
-	/* analog scan */
-	/* sample rate */
-	freq=1000.0;
-	/* nSamples */
-	count= 1000; // * NCHAN_USB1608FS;
-	/* PGA gain (10V, 5V, 2V, 1V) */
-	/*
-        case 1: gain = BP_10_00V;
-        case 2: gain = BP_5_00V;
-        case 4: gain = BP_2_00V;
-        case 6: gain = BP_1_00V;
-	*/
 
 	usbAInStop_USB1608FS(udev);
 
@@ -176,12 +93,13 @@ int main (int argc, char **argv) {
 	//options = AIN_EXECUTION | AIN_DEBUG_MODE;
 
 	/* start the scan */
-	fprintf(stderr,"# starting analog scan (frequency=%.f count=%d)\n",freq,count);
-	usbAInScan_USB1608FS(udev, 0, 7, count, &freq, options, data);
+	float freq = DAQ_SAMPLE_FREQ;
+	fprintf(stderr,"# starting analog scan (frequency=%.f count=%d)\n",freq,DAQ_SAMPLE_COUNT);
+	usbAInScan_USB1608FS(udev, 0, 7, DAQ_SAMPLE_COUNT, &freq, options, data);
 	/* done with acquire, now we know how it actually did */
 	fprintf(stderr,"# DAQ scan complete (actual frequency = %f)\n", freq);
 
-	for ( nSample=0 ; nSample < count ; nSample++ ) {
+	for ( nSample=0 ; nSample < DAQ_SAMPLE_COUNT ; nSample++ ) {
 		for ( int nChannel=0; nChannel < NCHAN_USB1608FS ; nChannel++ ) {
 			uint16_t dat;	/* raw 16 bit value from ADC */
 			int16_t sdat;	/* scaled using DAQ calibration and gain and sign extended */
@@ -223,22 +141,22 @@ int main (int argc, char **argv) {
 				ch[nChannel].vMax=volts;
 			}
 
-			double value = ch[nChannel].m * volts + ch[nChannel].b;
+//			double value = ch[nChannel].m * volts + ch[nChannel].b;
 //			fprintf(stderr,"# value=%.4f raw data = %#x   data[%d] = %#hx  %.4fV\n", value, data[i], i, sdat, volts);
 //			fprintf(stderr,"%d[%d], %.4f, %d, %d, %.4f\n",nSample,nChannel,value,data[nSample*8+nChannel],sdat,volts);
 
 			if ( CHANNEL_MODE_FREQUENCY_FROM_ANALOG == ch[nChannel].mode ) {
-				printf("frequency_from_analog[%d] - nSample=%d - volts=%0.1f - ",nSample,nChannel,volts);
+//				printf("frequency_from_analog[%d] - nSample=%d - volts=%0.1f - ",nSample,nChannel,volts);
 				if ( 1==ch[nChannel].lastLogicLevel && volts <= VOLTAGE_LOGIC_LOW ) {
 					/* were logic level high, now are low ... got a falling edge */
 					ch[nChannel].lastLogicLevel=0;
 					ch[nChannel].nFallingEdges++;
-					printf("now low and was high ... falling edge\n");
+//					printf("now low and was high ... falling edge\n");
 				} else if ( volts >= VOLTAGE_LOGIC_HIGH ) {
 					ch[nChannel].lastLogicLevel=1;
-					printf("high\n");
+//					printf("high\n");
 				} else {
-					printf("no action\n");
+//					printf("no action\n");
 				}
 			}
 		}
@@ -250,7 +168,7 @@ int main (int argc, char **argv) {
 	for ( i=0 ; i<NCHAN_USB1608FS ; i++ ) {
 		if ( CHANNEL_MODE_FREQUENCY_FROM_ANALOG == ch[i].mode ) {
 			/* calculate frequency based on our gate time */
-			ch[i].frequency = ch[i].nFallingEdges / (count / freq);
+			ch[i].frequency = ch[i].nFallingEdges / ( ch[i].nSamples / freq);
 
 			fprintf(stderr,"#\t[%d] %d frequency=%0.1f\n",i,ch[i].nFallingEdges,ch[i].frequency);
 		}
@@ -260,6 +178,127 @@ int main (int argc, char **argv) {
 		fprintf(stderr,"#\t[%d] vMin=%0.4f vMax=%0.4f vAvg=%0.4f nSamples=%d\n",i,ch[i].vMin,ch[i].vMax,ch[i].vAvg,ch[i].nSamples);
 	}
 
+
+	return ch[0].nSamples;
+}
+
+int main (int argc, char **argv) {
+	int i;
+	int ret;
+
+
+	/* configure our channels */
+	init_channels();
+	/* rectifier DC voltage */
+	ch[0].mode=CHANNEL_MODE_ANALOG;
+	ch[0].m=20.0;
+	ch[0].b=0.0;
+	ch[0].gain=BP_5_00V;
+
+	/* rectifier DC current */
+	ch[1].mode=CHANNEL_MODE_ANALOG;
+	ch[1].m=1750.0;
+	ch[1].b=0.0;
+	ch[1].gain=BP_1_00V;
+
+	/* field DC voltage */
+	ch[2].mode=CHANNEL_MODE_ANALOG;
+	ch[2].m=20.0;
+	ch[2].b=0.0;
+	ch[2].gain=BP_5_00V;
+
+	/* field DC current */
+	ch[3].mode=CHANNEL_MODE_ANALOG;
+	ch[3].m=1750.0;
+	ch[3].b=0.0;
+	ch[3].gain=BP_1_00V;
+
+	/* turbine output frequency */
+	ch[4].mode=CHANNEL_MODE_FREQUENCY_FROM_ANALOG;
+	ch[4].m=1.0;
+	ch[4].b=0.0;
+	ch[4].gain=BP_5_00V;
+
+	/* dyno shaft RPM */
+	ch[5].mode=CHANNEL_MODE_FREQUENCY_FROM_ANALOG;
+	ch[5].m=1.0;
+	ch[5].b=0.0;
+	ch[5].gain=BP_5_00V;
+
+	/* turbine temperature via IR thermocouple */
+	ch[6].mode=CHANNEL_MODE_ANALOG;
+	ch[6].m=100.0;
+	ch[6].b=0.0;
+	ch[6].gain=BP_2_00V;
+
+	/* strain gauge on dyno output shaft */
+	ch[7].mode=CHANNEL_MODE_ANALOG;
+	ch[7].m=1.0;
+	ch[7].b=0.0;
+	ch[7].gain=BP_5_00V;
+
+
+	udev = 0;
+	ret = libusb_init(NULL);
+	if ( ret < 0 ) {
+		perror("libusb_init: Failed to initialize libusb");
+		exit(1);
+	}
+
+
+	if ( (udev = usb_device_find_USB_MCC(USB1608FS_PID, NULL)) ) {
+		fprintf(stderr,"# USB-1608FS device found\n");
+	} else {
+		fprintf(stderr,"# No USB-1608FS device found.\n");
+		exit(1);
+	}
+ 
+	fprintf(stderr,"# Downloading and building calibration table from device ...\n");
+	usbBuildCalTable_USB1608FS(udev, table_AIN);
+	fprintf(stderr,"# calibration table downloaded and built.\n");
+
+#if 0
+	/* print the table */
+	for ( i=0 ; i < NGAINS_USB1608FS; i++) {
+		for ( int j = 0; j < NCHAN_USB1608FS; j++) {
+			fprintf(stderr,"calibration table_AIN[%d][%d].slope,offset = %f,%f\n", i, j, table_AIN[i][j].slope, table_AIN[i][j].offset);
+		}	
+	}
+#endif
+
+
+	/* USB-1608FS DAQ is ready to use */
+
+
+
+
+	for ( i=0 ; i<20 ; i++ ) {
+		/* start new measurement */
+		fprintf(stderr,"##################################################################################\n");
+		init_channel_stats();
+
+		/* command dyno RPM */
+		fprintf(stderr,"# setting dyno RPM\n");
+
+		/* wait for dyno RPM to stabilize */
+		fprintf(stderr,"# delay to allow RPM to be reached\n");
+		sleep(1);
+
+		/* acquire data */
+		fprintf(stderr,"# acquiring data\n");
+		daq_acquire();
+
+		/* send / save data */
+		fprintf(stderr,"# exporting data\n");
+	}
+
+
+
+
+
+
+
+	/* clean up MCC USB-1608FS DAQ */
 	usbAInStop_USB1608FS(udev);
 	usbReset_USB1608FS(udev);
 
